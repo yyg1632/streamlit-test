c68c25ff532ca3a645bf74c71144a068d84dc8f4
yyg1632 <60237183+yyg1632@users.noreply.github.com>
2023-01-15 22:26:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.write('# RF ON TIME')
-data = pd.read_csv('C:/Users/윤영길/Desktop/Python/PythonWorkspace/data_science/dash_test1.csv')
+data = pd.read_csv('dash_test1.csv')
 st.write('## raw')
 data
 
