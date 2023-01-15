9c3638fd128e942269c32986ab52297ca60d4f57
yyg1632 <60237183+yyg1632@users.noreply.github.com>
2023-01-15 21:54:43 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+
+st.write('# RF ON TIME')
+data = pd.read_csv('C:/Users/윤영길/Desktop/Python/PythonWorkspace/data_science/dash_test1.csv')
+st.write('## raw')
+data
+
